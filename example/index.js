f8ab503944cf8b1d4d82ef34d299396b86e7ad13
Carlos Sánchez <carlos@typeform.com>
2016-01-08 11:51:52 +0100

    create project skeleton

---
+++ example/index.js
@@ -0,0 +1,8 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Toogle = require('../lib/index')
+
+ReactDOM.render(
+  React.createElement(Toogle),
+  document.getElementById('root')
+)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Toogle extends Component {
+  render () {
+    return <div>Toogle</div>
+  }
+}
+
+export default Toogle
