93e4aae86e36faf84224aae600824c08c8294f23
Carlos Sánchez <carlos@typeform.com>
2016-01-08 18:22:54 +0100

    prepare github page

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,8 +2,6 @@ var path = require('path')
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
     filename: 'bundle.js'
   },
   module: {
